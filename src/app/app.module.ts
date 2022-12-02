0971d49f5a4df793d9006f722ac8bc6f207f2c57
Bertin Nicolas <nicolas.bertin.dev@gmail.com>
2022-12-02 22:35:46 +0100

    build: packages installation
    - Installed popper, bootstrap
    - Added Http handling module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
