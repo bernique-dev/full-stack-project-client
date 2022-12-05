3b2e287370ea62f3fce105394ececa21cf79847b
Antoine Denise <denise.antoine71@gmail.com>
2022-12-05 15:21:18 +0100

    feat: access to shops endpoint with service

---
+++ src/app/shop.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Shop} from "./shared/shop";
+import {environment} from "../environments/environment";
+
+@Injectable()
+export class ShopService {
+
+  constructor(private http : HttpClient) { }
+
+  getShops() : Observable<Shop[]> {
+    return this.http.get<Shop[]>(environment.shopsUrl);
+  }
+
+}
